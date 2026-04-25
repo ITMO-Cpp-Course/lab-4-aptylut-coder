be464893c96ffdf0adea505de7974b74c07005e5
aptylut-coder <apty.lut@gmail.com>
2026-04-25 04:03:39 +0300

    first commit

---
+++ src/resource_core/include/resource_core.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <stdexcept>
+#include <string>
+#include <memory>
+#include <unordered_map>
+
+
+namespace lab4::resource {
+
+class ResourceError : public std::runtime_error {
+public:
+    explicit ResourceError(const std::string& message);
+};
+
+class FileHandle {
+private:
+    std::FILE* file_;
+
+public:
+
+    explicit FileHandle(const std::string& filename);
+
+
+    ~FileHandle();
+
+
+    FileHandle(const FileHandle&) = delete;
+    FileHandle& operator=(const FileHandle&) = delete;
+
+
+    FileHandle(FileHandle&& other) noexcept;
+    FileHandle& operator=(FileHandle&& other) noexcept;
+
+    bool isOpen() const;
+};
+
+class ResourceManager {
+private:
+    std::unordered_map<std::string, std::weak_ptr<FileHandle>> cache_;
+
+public:
+    std::shared_ptr<FileHandle> getFile(const std::string& filename);
+};
+
+} // namespace lab4::resource
+
